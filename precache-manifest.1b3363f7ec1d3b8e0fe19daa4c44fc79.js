ba1279ecb93d5e466961d93fe8100f637c791b15
aisprilet <nidhi.ales@gmail.com>
2024-10-17 13:25:31 +0530

    Updates

---
+++ precache-manifest.1b3363f7ec1d3b8e0fe19daa4c44fc79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6919f099211e30e56d5de01a47164517",
+    "url": "/kanban-baord/index.html"
+  },
+  {
+    "revision": "becef41fe5891ff37b88",
+    "url": "/kanban-baord/static/css/main.a016a4fd.chunk.css"
+  },
+  {
+    "revision": "5ab1d717761216b2c4ba",
+    "url": "/kanban-baord/static/js/2.6924b1ab.chunk.js"
+  },
+  {
+    "revision": "65a9926ecde5afbe3bda",
+    "url": "/kanban-baord/static/js/3.3b6fb9ae.chunk.js"
+  },
+  {
+    "revision": "becef41fe5891ff37b88",
+    "url": "/kanban-baord/static/js/main.5e763b5c.chunk.js"
+  },
+  {
+    "revision": "5b48816a879cba66b067",
+    "url": "/kanban-baord/static/js/runtime~main.a4867669.js"
+  }
+]);
